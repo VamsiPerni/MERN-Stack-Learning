e8b8391a0670913c333978f5e0fe27d1a5d7c5db
VamsiPerni <mohanperni777@gmail.com>
2025-06-30 18:32:38 +0530

    Assignment Questions Discussion and doubt clearning

---
+++ Day-10/src/App.jsx
@@ -0,0 +1,61 @@
+//---------------------------------------------------------
+
+// import { useState } from "react";
+
+// const arr = ["kiwi", "banana", "pineapple"];
+
+// const App = () => {
+//   const [editIdx, setEditIdx] = useState(-1);
+
+//   return (
+//     <div>
+//       {arr.map((elem, idx) => {
+//         return (
+//           <div key={idx}>
+//             {idx == editIdx ? <input type="text" /> : <h2>{elem}</h2>}
+//             <button
+//               onClick={() => {
+//                 setEditIdx(idx);
+//               }}
+//             >
+//               Edit
+//             </button>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+//---------------------------------------------------------
+
+// import { useState } from "react";
+
+const FRUITS = [
+  {
+    name: "Kiwi",
+    color: "green",
+    price: 600,
+  },
+  {
+    name: "Banana",
+    color: "yellow",
+    price: 40,
+  },
+  {
+    name: "Apple",
+    color: "red",
+    price: 200,
+  },
+  {
+    name: "Mango",
+    color: "yellow",
+    price: 150,
+  },
+];
+
+const App = () => {};
+
+export default App;
