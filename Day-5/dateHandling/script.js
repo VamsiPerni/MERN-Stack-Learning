425c83adfa77f3c941c667d605edb8c93373ae82
VamsiPerni <mohanperni777@gmail.com>
2025-06-30 18:11:47 +0530

    Add initial HTML and JavaScript files for date handling feature

---
+++ Day-5/dateHandling/script.js
@@ -0,0 +1,24 @@
+console.log("Hello");
+
+// -----------------------------
+
+// const newDate = new Date();
+// console.log("Today's Date : ", newDate); // we will get date in ISO Format
+
+// -----------*WRONG*--------------------
+
+// let newDate = null;
+
+// const handleDateChange = (e) => {
+//   newDate = newDate(e.target.value);
+//   console.log("newDate : ", newDate);
+
+//   const targetMilliseconds = newDate.getTime();
+//   console.log(" targetMilliseconds ", targetMilliseconds);
+
+//   const currentTimeInMilliseconds = Date.now();
+//   console.log("currentTimeInMilliseconds : ", currentTimeInMilliseconds);
+
+//   const diffInSeconds = (targetMilliseconds - currentTimeInMilliseconds) / 1000;
+//   console.log("diffInSeconds : ", diffInSeconds);
+// };
